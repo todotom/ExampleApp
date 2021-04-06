8d7721c5fe47265c96ac58507d1d68be7a4bef9c
Tomás Darío Albarenque Malnero <tomasdarioam@protonmail.com>
2021-04-06 02:22:42 +0200

    Upgrading security and adding local development environment

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, Blueprint, request, jsonify
 from flask_cors import CORS
 
+import os
+
 import json
 import mariadb
 
 database_config = {
-    'host': '127.0.0.1',
+    'host': 'database',
     'port': 3306,
     'user': 'root',
-    'password': 'Password123!',
+    'password': os.environ['DB_PASSWORD'],
     'database': 'inventory'
 }
 
@@ -37,20 +39,18 @@ def handle_submit():
 @api.route('/items', methods=['POST'])
 def handle_item_post():
     if request.method == "POST":
-        first_name = request.form['firstName']
-        last_name = request.form['lastName']
-        job = request.form['job']
-        print(f'first name : {first_name}')
-        print(f'last name : {last_name}')
-        print(f'job : {job}')
+        name = request.form['name']
+        description = request.form['description']
+        price = request.form['price']
 
 @api.route('/items', methods=['GET'])
 def handle_items_get():
     if request.method == "GET":
-        connection = mariadb.connect(**database_config)
-        
+        #connection = mariadb.connect(**database_config)
+        connection = connect_database()
+    
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM items");
+        cursor.execute("SELECT * FROM items")
 
         row_headers=[x[0] for x in cursor.description]
         rv = cursor.fetchall()
@@ -61,6 +61,9 @@ def handle_items_get():
         return json.dumps(json_data)
 
 app.register_blueprint(api, url_prefix='/api')
+
+def connect_database():
+    return mariadb.connect(**database_config)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5050)
